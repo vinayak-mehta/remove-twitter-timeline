f520412ab7130057f95fb7c3bb1ee87552b65c5d
Vinayak Mehta <vmehta94@gmail.com>
2020-07-12 03:13:29 +0530

    Fix #1: Run script when document is idle

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function handleUpdated(tabId, changeInfo, tabInfo) {
   if(changeInfo.url == "https://twitter.com/home") {
     browser.tabs.executeScript(tabId, {
       file: "remove-timeline.js",
-      runAt: "document_end"
+      runAt: "document_idle"
     });
   }
 }
