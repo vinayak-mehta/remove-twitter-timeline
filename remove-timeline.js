d3d33a361e6b78027db3da2dcd491f324bee0e41
Vinayak Mehta <vmehta94@gmail.com>
2021-01-10 04:16:15 +0530

    Remove What's Happening

--- remove-timeline.js
+++ remove-timeline.js
@@ -5,8 +5,11 @@ function getElementByXpath(path) {
 }
 
 function checkTimeline() {
-  const timeline = getElementByXpath("/html/body/div/div/div/div[2]/main/div/div/div");
   const atHome = window.location.href.includes("twitter.com/home")
+  const timeline = getElementByXpath("/html/body/div/div/div/div[2]/main/div/div/div");
+
+  const whatsHappening = getElementByXpath("/html/body/div/div/div/div[2]/main/div/div/div/div[2]");
+  whatsHappening.style.visibility = "hidden";
 
   if (timeline != null) {
     timeline.style.visibility = atHome ? "hidden" : "visible";
